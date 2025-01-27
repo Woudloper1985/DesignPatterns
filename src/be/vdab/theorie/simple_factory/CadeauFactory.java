93164f87cc15d3ed03ec6d6fbcbbe7710e61c4c2
NickVandenbroeck <nick.vandenbroeck@vdabcampus.be>
2025-01-27 12:06:23 +0100

    Simple Factory 1.1

--- src/be/vdab/theorie/simple_factory/CadeauFactory.java
+++ src/be/vdab/theorie/simple_factory/CadeauFactory.java
@@ -7,7 +7,7 @@ enum CadeauFactory {
         if (leeftijd <= 5) {
             return new Pop(20.50);
         }
-        if (leeftijd >= 6 && leeftijd <= 12) {
+        if (leeftijd <= 12) {
             return new Gezelschapsspel(40.99);
         }
         return new Boekenbon(60);
